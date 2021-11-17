227cb889c446815a27313da48911927e0f7fe102
dinhtheanh998 <dinhtheanh998@gmail.com>
2021-11-17 18:41:13 +0700

    Đức

---
+++ BTL_LTQL/BTL_LTQL/Scripts/main.js
@@ -0,0 +1,12 @@
+﻿const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const menu = $$(".menu-item");
+menu.forEach(function (course, index) {
+    course.onclick = function () {
+        $(".menu-item.active").classList.remove("active");
+        console.log(menu)
+
+        this.classList.add("active");
+    };
+});
